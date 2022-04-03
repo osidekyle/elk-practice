a5141b2739e4a95b813c65a246d6ec923b2b65e8
osidekyle <hawkinskyle67@gmail.com>
2022-04-03 12:36:11 -0700

    geo point example

--- main3.py
+++ main3.py
@@ -3,3 +3,28 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch([{'host' : 'localhost', 'port' : 9200, 'scheme' : 'http'}])
 
+Settings = {
+    'settings': {
+        'number_of_shards':1,
+        'number_of_replicas':0
+    },
+    'mappings':{
+    'properties':{
+        'myloc':{
+            'type':'geo_point'
+        }
+    }
+    }
+}
+
+
+my = es.indices.create(index="hawkins", ignore=400, body=Settings)
+
+sample = {
+    'myloc':{
+        'lat':48.75,
+        'lon':-122.48
+    }
+}
+
+res1 = es.index(index="hawkins", body=sample, id=1)
